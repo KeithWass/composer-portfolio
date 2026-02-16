a9e5f8014460e699613bff2d085a14b463a3b9b9
Keith <keith@Keiths-MacBook-Pro.local>
2026-02-16 09:37:37 +0000

    changed font line height

--- Keith Wass Composer/src/App.jsx
+++ Keith Wass Composer/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Sidebar } from "./components/SideBar";
 import buildings from "../src/assets/buildings.jpg";
+import street from "../src/assets/sunnyStreet.jpg";
 import "./App.css";
 
 function App() {
@@ -9,14 +10,14 @@ function App() {
   return (
     <div className="app">
       <button className="menu-button" onClick={() => setIsOpen(!isOpen)}>
-        = menu
+        = MENU
       </button>
       <Sidebar isOpen={isOpen} />
 
       <main className="main" style={{ backgroundImage: `url(${buildings})` }}>
         <div className="building-text">
           <h1>K E I T H&nbsp; &nbsp;W A S S </h1>
-          <h2>C O M P O S E R </h2>
+          <h2> C O M P O S E R</h2>
         </div>
       </main>
     </div>
